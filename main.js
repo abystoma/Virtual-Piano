3f5bb040ebfedb7e20fecf3e81ffdbdd284aa53f
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-01-12 00:19:39 +0600

    Added audio to white keys

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const pianoKeys = ["A", "S", "D", "F", "G", "H", "J"];
 
 document.addEventListener("keypress", (event) => {
   const keyUpper = event.key.toUpperCase();
+  //let audio = new Audio(`keysounds/${keyUpper}.mp3`);
+  let audio = new Audio(document.getElementById(keyUpper).src);
 
   pianoKeys.includes(keyUpper)
-    ? console.log(`The '${keyUpper}' key is pressed.`)
+    ? audio.play()
     : console.warn("Wrong key is pressed.");
 });
 //event.code when you do not care about case and vice versa, use event.key when you care about case
